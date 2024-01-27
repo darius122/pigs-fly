e44f1fdbae2d921cdc669b3992023d37e7046e98
SShura04 <sebastiantwj@gmail.com>
2024-01-27 18:33:02 +0800

    v 0.8
    added parrelax

---
+++ Assets/Scripts/parallaxLayer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class parallaxLayer : MonoBehaviour
+{
+    public float parallaxFactor;
+
+    public void Move(float delta)
+    {
+        Vector3 newPos = transform.localPosition;
+        newPos.x -= delta * parallaxFactor;
+
+        transform.localPosition = newPos;
+    }
+
+}
