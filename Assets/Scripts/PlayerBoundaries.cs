041b4c5250dddffddaa0a02c5e4ee1314609a53c
darius122 <88877663+darius122@users.noreply.github.com>
2024-01-27 16:47:37 +0800

    v0.03
    - added player jumping
    - added pig sprite

--- Assets/Scripts/PlayerBoundaries.cs
+++ Assets/Scripts/PlayerBoundaries.cs
@@ -7,20 +7,42 @@ public class PlayerBoundaries : MonoBehaviour
     private Vector2 screenBounds;
     private float objectWidth;
     private float objectHeight;
+    private Rigidbody2D rb;
+
+    [SerializeField] private float gravity;
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = gravity;
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         objectWidth = GetComponent<SpriteRenderer>().bounds.size.x / 2;
         objectHeight = GetComponent<SpriteRenderer>().bounds.size.y / 2;
     }
-
-    // Update is called once per frame
+   
+    private void Update()
+    {
+        CheckCollisionWithBounds();
+    }
     void LateUpdate()
     {
         Vector3 viewPos = transform.position;
         viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
         viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
         transform.position = viewPos;
+    }
+    private void CheckCollisionWithBounds()
+    {
+        //if touching the top
+        if (transform.position.y > screenBounds.y - objectHeight )
+        {
+            rb.velocity = Vector2.zero;
+        }
+        if (transform.position.y < -screenBounds.y + objectHeight)
+        {
+            rb.gravityScale = 0;
+        }
+        else rb.gravityScale = gravity;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,16 +11,20 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetMouseButtonDown(0))
             Jump();
+        //Debug.Log(rb.velocity);
     }
     private void Jump()
     {
-        rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        rb.velocity = Vector2.zero;
+        rb.AddForce(Vector2.up * jumpForce);
     }
 }
