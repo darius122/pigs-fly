ab31339d9b1152f0dc12c54ff2665fa11c726e0f
WashPond <ariwardenny23@gmail.com>
2024-01-27 19:10:40 +0800

    updated dialoguemanager for ian dimitri

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -7,56 +7,87 @@ public class DialogueManager : MonoBehaviour
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI dialogueText;
     public Animator dialogueAnim;
-
+    int aa;
+    Dialogue[] dialogueVal;
     private Queue<string> sentences;
     // Start is called before the first frame update
     void Start()
     {
-        sentences = new Queue<string>();   
+        sentences = new Queue<string>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             DisplayNextSentence();
         }
     }
 
-    public void StartDialogue(Dialogue dialogue)
+    public void StartDialogue(Dialogue[] dialogue)
     {
+        dialogueVal = dialogue;
         dialogueAnim.SetBool("IsOpen", true);
 
-        nameText.text = dialogue.name;
 
+        //Debug.Log(i);
+        nameText.text = dialogue[aa].name;
         sentences.Clear();
 
-        foreach(string sentence in dialogue.sentences)
+        foreach (string sentence in dialogue[aa].sentences)
         {
             sentences.Enqueue(sentence);
         }
-
         DisplayNextSentence();
+
+
+
+
+
     }
+
 
     public void DisplayNextSentence()
     {
-        if(sentences.Count == 0)
+        if (sentences.Count == 0)
         {
-            EndDialogue();
-            return;
+            aa ++;
+            if (NextPerson())
+            {
+                return;
+            }
         }
 
         string sentence = sentences.Dequeue();
 
-        StopAllCoroutines(); 
-        StartCoroutine(TypeSentence(sentence)); 
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    bool NextPerson()
+    {
+        if (aa >= dialogueVal.Length -1)
+        {
+            EndDialogue();
+            return true;
+        }
+
+        nameText.text = dialogueVal[aa].name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogueVal[aa].sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+        return false;
     }
 
     void EndDialogue()
     {
          dialogueAnim.SetBool("IsOpen", false);
+         aa++;
     }
 
     IEnumerator TypeSentence(string sentence)
