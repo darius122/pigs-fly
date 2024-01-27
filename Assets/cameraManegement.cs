60e296128d3d293ec9ffed5799c6558351bad79c
darius122 <88877663+darius122@users.noreply.github.com>
2024-01-27 17:10:09 +0800

    v0.04
    - fix junkai shitty code

--- Assets/cameraManegement.cs
+++ Assets/cameraManegement.cs
@@ -9,29 +9,22 @@ public class cameraManegement : MonoBehaviour
 
     [SerializeField] CinemachineVirtualCamera a;
     [SerializeField]PlayerBoundaries PB;
-    int b = 0;
     [SerializeField] float currentOrthSize;
     // Start is called before the first frame update
     void Start()
     {
         currentOrthSize = a.m_Lens.OrthographicSize;
 
-        StartCoroutine(resize());
+        StartCoroutine(Resize());
     }
 
-    IEnumerator resize()
+    IEnumerator Resize()
     {
         yield return new WaitForSeconds(4);
-        ExpanView(5);
-        StartCoroutine(resize());
+        ExpandView(5);
+        StartCoroutine(Resize());
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-     
-    }
-    void ExpanView(float a)
+    void ExpandView(float a)
     {
         DOTween.To(() => currentOrthSize, x => currentOrthSize = x, currentOrthSize+a, 1f).OnUpdate(() => {
 
