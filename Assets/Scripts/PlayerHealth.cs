a09ea8d04d010f954eb6c4ddcc872de5b753ba39
darius122 <88877663+darius122@users.noreply.github.com>
2024-01-28 16:45:25 +0800

    dfssdffasd

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,29 +7,16 @@ using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
     private int health = 2;
-    public List<Image> heartSprites = new List<Image>();
     // Start is called before the first frame update
     void Start()
     {
-        populateHealthSprites(health);
-    }
-    private void populateHealthSprites(int currHealth)
-    {
-        foreach(Image image in heartSprites)
-        {
-            image.gameObject.SetActive(false);
-        }
-        for(int i = 0; i < currHealth; i++)
-        {
-            heartSprites[i].gameObject.SetActive(true);
-        }
+       
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Object"))
         {
             health--;
-            populateHealthSprites(health);
             Destroy(collision.gameObject);
         }
     }
